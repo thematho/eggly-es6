92382ecf20b3165b62278498feb1ce4c17f38a5c
Matias Fernandez Martinez <thematho@gmail.com>
2016-10-24 00:08:04 -0300

    Exercise

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,12 @@
-console.log('Hello Eggly ES6!');
+import 'bootstrap-css-only';
+import 'normalize.css';
+
+import angular from 'angular';
+import appComponent from './app.component';
+
+/**
+* app Module
+*
+* Application Main Module
+*/
+angular.module('app', []).component('appComponent', appComponent);
